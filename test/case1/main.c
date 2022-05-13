817ab71fb0d76721fbb1e8ce528741a82f6407fe
맹주영 <mjy9088@naver.com>
2022-05-14 05:06:42 +0900

    fix norm

--- test/case1/main.c
+++ test/case1/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jmaing <jmaing@student.42seoul.kr>         +#+  +:+       +#+        */
+/*   By: Juyeong Maing <jmaing@student.42seoul.kr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 15:00:12 by jmaing            #+#    #+#             */
-/*   Updated: 2022/05/14 02:49:20 by jmaing           ###   ########.fr       */
+/*   Updated: 2022/05/14 05:06:31 by Juyeong Maing    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,14 @@
 #include <stdio.h>
 
 #include "../../src/leak_test.h"
+
+static void	*a(size_t size)
+{
+	void *const	result = malloc(size);
+
+	free(result);
+	return (result);
+}
 
 static const char	*b(void *ptr)
 {
@@ -44,12 +52,9 @@ bool	test(void *context)
 		printf("%s %s\n", b(p1), b(p2));
 		return (false);
 	}
-	p3 = malloc(42);
-	p4 = malloc(42);
-	p5 = malloc(42);
-	free(p3);
-	free(p4);
-	free(p5);
+	p3 = a(42);
+	p4 = a(42);
+	p5 = a(42);
 	leak_test_end();
 	printf("%s %s %s %s %s\n", b(p1), b(p2), b(p3), b(p4), b(p5));
 	return (false);
